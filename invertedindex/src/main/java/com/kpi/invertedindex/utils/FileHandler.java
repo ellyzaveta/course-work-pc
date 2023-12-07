5a8a8b9747210a644b83bd4d3791d71c1e99bcef
ellyzaveta <lizamari21@gmail.com>
2023-12-07 17:44:00 -0500

    feat[invertedindex]: add util for file handling

---
+++ invertedindex/src/main/java/com/kpi/invertedindex/utils/FileHandler.java
@@ -0,0 +1,38 @@
+package com.kpi.invertedindex.utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileHandler {
+
+    public static List<Path> getAllTxtFilesPaths(String directoryPath) throws IOException {
+        try (Stream<Path> paths = Files.walk(Paths.get(directoryPath))) {
+            return paths
+                    .filter(Files::isRegularFile)
+                    .filter(path -> path.toString().endsWith(".txt"))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    public static long getFileSize(Path path) {
+        try {
+            return Files.size(path);
+        } catch (IOException e) {
+            System.err.println("Unable to get size of file: " + path);
+            return 0;
+        }
+    }
+
+    public static long getFileListSize(List<Path> filePathsToIndex) {
+        return filePathsToIndex.stream().mapToLong(FileHandler::getFileSize).sum();
+    }
+
+    public static String getStringPath(Path path) {
+        return path.toString();
+    }
+}
