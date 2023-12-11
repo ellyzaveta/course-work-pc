bfb6259aa02db0270a8cb75efc6c65ba4a21a5df
ellyzaveta <lizamari21@gmail.com>
2023-12-11 15:26:47 -0500

    feat[performancecomparison]: add configuration

---
+++ performancecomparison/src/main/java/com/kpi/performancecomparison/configuration/SpringConfiguration.java
@@ -0,0 +1,27 @@
+package com.kpi.performancecomparison.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("com.kpi")
+@PropertySource("classpath:application.properties")
+public class SpringConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
